4c6a25ea8857017405c59f29ad510e4d3b00a378
poojaiiit17 <poojaiiit17@gmail.com>
2026-09-06 20:57:52 +0530

    Add Book model

---
+++ src/main/java/com/library/model/Book.java
@@ -0,0 +1,34 @@
+package com.library.model;
+
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private String category;
+    private int totalCopies;
+    private int availableCopies;
+
+    public Book() {}
+
+    public Book(int bookId, String title, String author, String category, int totalCopies, int availableCopies) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.totalCopies = totalCopies;
+        this.availableCopies = availableCopies;
+    }
+
+    public int getBookId() { return bookId; }
+    public void setBookId(int bookId) { this.bookId = bookId; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+    public int getTotalCopies() { return totalCopies; }
+    public void setTotalCopies(int totalCopies) { this.totalCopies = totalCopies; }
+    public int getAvailableCopies() { return availableCopies; }
+    public void setAvailableCopies(int availableCopies) { this.availableCopies = availableCopies; }
+}
